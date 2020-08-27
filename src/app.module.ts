81aadc852dd2f13bef2b87fcee34a38c032da8b4
Sergey <fhlbc2012@gmail.com>
2020-08-27 05:54:00 +0300

    - added user and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MongooseModule} from "@nestjs/mongoose";
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
