2f345aa7ce3e098e9da48b33d454fec218298429
Sergey <fhlbc2012@gmail.com>
2020-08-27 05:55:33 +0300

    - added providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
 import {MongooseModule} from "@nestjs/mongoose";
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import {UserModule} from './user/user.module';
+import {AuthModule} from './auth/auth.module';
+import {UserService} from "./user/user.service";
+import {AuthService} from "./auth/auth.service";
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule, AuthModule],
+    controllers: [AppController],
+    providers: [UserService, AuthService],
 })
-export class AppModule {}
+export class AppModule {
+}
